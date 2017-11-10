b81886e07def8e24902d86f1c0b8c47983bde32f
benjaminbowen00 <benjibowen@hotmail.com>
2017-11-10 14:24:24 +0000

    with limit on the number of people in room

--- room.rb
+++ room.rb
@@ -1,14 +1,20 @@
 class Room
   attr_reader :name, :guests, :songs
+  attr_accessor :capacity
 
   def initialize(name)
     @name = name
     @guests = []
     @songs = []
+    @capacity = 5
   end
 
   def check_in_guest(guest)
-    @guests << guest
+    if @guests.length < @capacity
+      @guests << guest
+    else
+      return "Sorry you have to wait for someone to leave"
+    end
   end
 
   def check_out_guest(guest)
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -10,6 +10,9 @@ class TestRoom < MiniTest::Test
     @guest1 = Guest.new("Mick")
     @guest2 = Guest.new("Christine")
     @guest3 = Guest.new("John")
+    @guest4 = Guest.new("Stevie")
+    @guest5 = Guest.new("Paul")
+    @guest6 = Guest.new("Ringo")
     @song1 = Song.new("Sara", "Fleetwood Mac")
     @song2 = Song.new("Song 2", "Blur")
     @room1 = Room.new("main")
@@ -47,5 +50,36 @@ class TestRoom < MiniTest::Test
    @room1.add_song(@song2)
    assert_equal([@song1, @song2], @room1.songs)
  end
+
+ def test_capacity
+   assert_equal(5, @room1.capacity)
+ end
+
+ def test_change_capacity
+   @room1.capacity = 10
+   assert_equal(10, @room1.capacity)
+ end
+
+ def test_capacity_reached
+   @room1.check_in_guest(@guest1)
+   @room1.check_in_guest(@guest2)
+   @room1.check_in_guest(@guest3)
+   @room1.check_in_guest(@guest4)
+   @room1.check_in_guest(@guest5)
+   assert_equal("Sorry you have to wait for someone to leave", @room1.check_in_guest(@guest6))
+ end
+
+ def test_capacity_not_reached_if_someone_has_left
+   @room1.check_in_guest(@guest1)
+   @room1.check_in_guest(@guest2)
+   @room1.check_in_guest(@guest3)
+   @room1.check_in_guest(@guest4)
+   @room1.check_in_guest(@guest5)
+   @room1.check_out_guest(@guest3)
+   @room1.check_in_guest(@guest6)
+   assert_equal([@guest1, @guest2, @guest4, @guest5, @guest6], @room1.guests)
+ end
+
+
 
 end
