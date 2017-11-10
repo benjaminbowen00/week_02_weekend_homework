b50752bf3649cb62cb1c77b3fa71607f3d142677
benjaminbowen00 <benjibowen@hotmail.com>
2017-11-10 16:04:56 +0000

    working up to cheer with checks for person in room, song in room and favourite song matches

--- guest.rb
+++ guest.rb
@@ -1,10 +1,20 @@
 class Guest
   attr_reader :name
-  attr_accessor :money
+  attr_accessor :money, :favourite_song
 
   def initialize(name)
     @name = name
     @money = 50
+    @favourite_song = nil
+  end
+
+  def cheer(room)
+    return if !room.guests.include?(self)
+    if (room.songs.include?(@favourite_song))
+      return "OMG I love #{@favourite_song.title}"
+    else
+      return "Wahey! I love caraoke"
+    end
   end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -32,71 +32,92 @@ class TestRoom < MiniTest::Test
   end
 
 
- def test_add_guests
-   @guest1.money = 4
-   @room1.check_in_guest(@guest1)
-   @room1.check_in_guest(@guest2)
-   assert_equal([@guest2], @room1.guests)
-   assert_equal(4, @guest1.money)
-   assert_equal(45, @guest2.money)
-   assert_equal(1005, @room1.money)
- end
+  def test_add_guests
+    @guest1.money = 4
+    @room1.check_in_guest(@guest1)
+    @room1.check_in_guest(@guest2)
+    assert_equal([@guest2], @room1.guests)
+    assert_equal(4, @guest1.money)
+    assert_equal(45, @guest2.money)
+    assert_equal(1005, @room1.money)
+  end
 
- def test_remove_guests
-   @room1.check_in_guest(@guest1)
-   @room1.check_in_guest(@guest2)
-   @room1.check_in_guest(@guest3)
-   @room1.check_out_guest(@guest2)
-   assert_equal([@guest1, @guest3], @room1.guests)
- end
+  def test_remove_guests
+    @room1.check_in_guest(@guest1)
+    @room1.check_in_guest(@guest2)
+    @room1.check_in_guest(@guest3)
+    @room1.check_out_guest(@guest2)
+    assert_equal([@guest1, @guest3], @room1.guests)
+  end
 
- def test_add_song
-   @room1.add_song(@song1)
-   @room1.add_song(@song2)
-   assert_equal([@song1, @song2], @room1.songs)
- end
+  def test_add_song
+    @room1.add_song(@song1)
+    @room1.add_song(@song2)
+    assert_equal([@song1, @song2], @room1.songs)
+  end
 
- def test_capacity
-   assert_equal(5, @room1.capacity)
- end
+  def test_capacity
+    assert_equal(5, @room1.capacity)
+  end
 
- def test_change_capacity
-   @room1.capacity = 10
-   assert_equal(10, @room1.capacity)
- end
+  def test_change_capacity
+    @room1.capacity = 10
+    assert_equal(10, @room1.capacity)
+  end
 
- def test_capacity_reached
-   @room1.check_in_guest(@guest1)
-   @room1.check_in_guest(@guest2)
-   @room1.check_in_guest(@guest3)
-   @room1.check_in_guest(@guest4)
-   @room1.check_in_guest(@guest5)
-   assert_equal("Sorry you can't come in", @room1.check_in_guest(@guest6))
- end
+  def test_capacity_reached
+    @room1.check_in_guest(@guest1)
+    @room1.check_in_guest(@guest2)
+    @room1.check_in_guest(@guest3)
+    @room1.check_in_guest(@guest4)
+    @room1.check_in_guest(@guest5)
+    assert_equal("Sorry you can't come in", @room1.check_in_guest(@guest6))
+  end
 
- def test_capacity_not_reached_if_someone_has_left
-   @room1.check_in_guest(@guest1)
-   @room1.check_in_guest(@guest2)
-   @room1.check_in_guest(@guest3)
-   @room1.check_in_guest(@guest4)
-   @room1.check_in_guest(@guest5)
-   @room1.check_out_guest(@guest3)
-   @room1.check_in_guest(@guest6)
-   assert_equal([@guest1, @guest2, @guest4, @guest5, @guest6], @room1.guests)
-   assert_equal(1030, @room1.money)
- end
+  def test_capacity_not_reached_if_someone_has_left
+    @room1.check_in_guest(@guest1)
+    @room1.check_in_guest(@guest2)
+    @room1.check_in_guest(@guest3)
+    @room1.check_in_guest(@guest4)
+    @room1.check_in_guest(@guest5)
+    @room1.check_out_guest(@guest3)
+    @room1.check_in_guest(@guest6)
+    assert_equal([@guest1, @guest2, @guest4, @guest5, @guest6], @room1.guests)
+    assert_equal(1030, @room1.money)
+  end
 
- def test_capacity_not_reached_limit_is_changed
-   @room1.capacity = 10
-   @room1.check_in_guest(@guest1)
-   @room1.check_in_guest(@guest2)
-   @room1.check_in_guest(@guest3)
-   @room1.check_in_guest(@guest4)
-   @room1.check_in_guest(@guest5)
-   @room1.check_in_guest(@guest6)
-   assert_equal(6, @room1.guests.length)
-   assert_equal([@guest1, @guest2, @guest3, @guest4, @guest5, @guest6], @room1.guests)
- end
+  def test_capacity_not_reached_limit_is_changed
+    @room1.capacity = 10
+    @room1.check_in_guest(@guest1)
+    @room1.check_in_guest(@guest2)
+    @room1.check_in_guest(@guest3)
+    @room1.check_in_guest(@guest4)
+    @room1.check_in_guest(@guest5)
+    @room1.check_in_guest(@guest6)
+    assert_equal(6, @room1.guests.length)
+    assert_equal([@guest1, @guest2, @guest3, @guest4, @guest5, @guest6], @room1.guests)
+  end
 
+  def test_cheer_no_favourite_song
+    @room1.check_in_guest(@guest1)
+    shout = @guest1.cheer(@room1)
+    assert_equal("Wahey! I love caraoke", shout)
+  end
 
+  def test_cheer_with_favourite_song_guest_not_in_room
+    @room1.add_song(@song2)
+    @guest1.favourite_song = @song2
+    shout = @guest1.cheer(@room1)
+    assert_nil(shout)
+  end
+
+  def test_cheer_with_favourite_song_in_room
+    @room1.check_in_guest(@guest1)
+    @room1.add_song(@song2)
+    @guest1.favourite_song = @song2
+    shout = @guest1.cheer(@room1)
+    assert_equal("OMG I love Song 2", shout)
+  end
+
+  
 end
